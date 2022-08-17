0ae5785009dd3d7fb7d81a7736ab2040179677bd
Mehdi Balouchi <mehdibalouchi@gmail.com>
2022-08-17 13:10:06 +0430

    Add interface for fxfs core api

---
+++ fxfs/core/iface/core.go
@@ -0,0 +1,34 @@
+package iface
+
+import (
+	"context"
+
+	files "github.com/ipfs/go-ipfs-files"
+	"github.com/ipfs/interface-go-ipfs-core/options"
+	"github.com/ipfs/interface-go-ipfs-core/path"
+)
+
+// Interfaces for fxfs
+
+type CoreAPI interface {
+	PrivateFS() PrivateFS
+
+	PublicFS() PublicFS
+}
+
+type PrivateFS interface {
+	Add(context.Context, files.Node, ...options.UnixfsAddOption) (path.Resolved, error)
+
+	// Get(context.Context, path.Path) (files.Node, error)
+
+	// Ls()
+
+}
+
+type PublicFS interface {
+	Add(context.Context, files.Node, ...options.UnixfsAddOption) (path.Resolved, error)
+
+	Get(context.Context, path.Path) (files.Node, error)
+
+	// Ls()
+}
