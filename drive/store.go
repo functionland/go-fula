67619989d57c02d4219b6f4267bb502400347320
Mehdi Balouchi <mehdibalouchi@gmail.com>
2022-08-24 16:23:52 +0430

    Refactor DriveStore so that: - it keeps the drives index in the memory - it implements the DriveStore comprising offering `Resolve`, `Put`, `Update` and `ResolveCreate` methods

--- drive/store.go
+++ drive/store.go
@@ -1,91 +1,57 @@
 package drive
 
 import (
-	"encoding/json"
 	"errors"
-	"fmt"
-	"io/ioutil"
-	"os"
 )
 
-const DRIVES_JSON_PATH = "./drives.json"
-
-func LoadDrivesJSON() (*Drives, error) {
-	drivesFile, err := os.Open(DRIVES_JSON_PATH)
-	if err != nil {
-		fmt.Println("could not open drives.json", err)
-		return nil, err
-	}
-
-	cdBytes, err := ioutil.ReadAll(drivesFile)
-	if err != nil {
-		fmt.Println("error reading drives.json", err)
-		return nil, err
-	}
-
-	var drives Drives
-	json.Unmarshal(cdBytes, &drives)
-
-	return &drives, nil
+type DriveIndex struct {
+	drives map[string]UserDrive
 }
 
-func WriteDrivesJSON(drives Drives) error {
-	ndFile, err := json.MarshalIndent(drives, "", " ")
-	if err != nil {
-		fmt.Println("error in writing new drives", err)
-		return err
+type DriveStore interface {
+	Resolve(string) (UserDrive, error)
+	ResolveCreate(string) (UserDrive, error)
+	Put(UserDrive) error
+	Update(UserDrive) error
+}
+
+func NewDriveStore() *DriveIndex {
+	return &DriveIndex{drives: make(map[string]UserDrive)}
+}
+
+func (di *DriveIndex) Resolve(userDID string) (UserDrive, error) {
+	if d, ok := di.drives[userDID]; ok {
+		return d, nil
 	}
 
-	err = ioutil.WriteFile(DRIVES_JSON_PATH, ndFile, 0644)
-	if err != nil {
-		fmt.Println("error in writing new drives", err)
-		return err
+	return UserDrive{}, errors.New("Drive not found")
+}
+
+func (di *DriveIndex) ResolveCreate(userDID string) (UserDrive, error) {
+	d, err := di.Resolve(userDID)
+	if err == nil {
+		return d, nil
 	}
 
+	d = NewDrive(userDID, di)
+	di.Put(d)
+	return d, nil
+}
+
+func (di *DriveIndex) Put(d UserDrive) error {
+	if _, ok := di.drives[d.UserDID]; ok {
+		return errors.New("drive already exists")
+	}
+
+	di.drives[d.UserDID] = d
 	return nil
 }
 
-func ResolveDrive(userDID string) (*UserDrive, error) {
-	drives, err := LoadDrivesJSON()
-	if err != nil {
-		fmt.Println("error in loading drives", err)
-		return nil, err
+func (di *DriveIndex) Update(d UserDrive) error {
+	if _, ok := di.drives[d.UserDID]; ok {
+		di.drives[d.UserDID] = d
+		return nil
 	}
 
-	for _, ud := range drives.Drives {
-		if ud.UserDID == userDID {
-			return &ud, nil
-		}
-	}
-
-	return NewDrive(userDID), errors.New("drive not found")
-}
-
-func PutDrive(ud UserDrive) error {
-	drives, err := LoadDrivesJSON()
-	if err != nil {
-		fmt.Println("error in loading drives", err)
-		return err
-	}
-
-	found := false
-	for idx, dr := range drives.Drives {
-		if dr.UserDID == ud.UserDID {
-			drives.Drives[idx].PrivateSpaceCid = ud.PrivateSpaceCid
-			drives.Drives[idx].PublicSpaceCid = ud.PublicSpaceCid
-			found = true
-		}
-	}
-
-	if !found {
-		drives.Drives = append(drives.Drives, ud)
-	}
-
-	// err = WriteDrivesJSON(*drives)
-	// if err != nil {
-	// 	fmt.Println("error in writing drivrd to json", err)
-	// 	return err
-	// }
-
-	return nil
+	return errors.New("drive does not exist")
 }
