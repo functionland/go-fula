93e60533e8d17639d0afbfa2e213ef1f54df3d42
Mehdi Balouchi <mehdibalouchi@gmail.com>
2022-09-04 22:19:18 +0430

    Add requester and handler for Write action Add reqSize sending before req send

--- protocols/newFile/requests.go
+++ protocols/newFile/requests.go
@@ -2,6 +2,7 @@ package newFile
 
 import (
 	"context"
+	"encoding/binary"
 	"io"
 
 	"github.com/golang/protobuf/proto"
@@ -11,29 +12,39 @@ import (
 
 var log = logging.Logger("newFilePL")
 
+func sendRequest(req *FSRequest, s network.Stream) error {
+	reqMsg, err := proto.Marshal(req)
+	if err != nil {
+		return err
+	}
+
+	// @TODO calculate the maximum bytes needed for sending the length of the req
+	sbuf := make([]byte, 2)
+	binary.LittleEndian.PutUint16(sbuf, uint16(len(reqMsg)))
+	n, err := s.Write(sbuf)
+	if err != nil {
+		return err
+	}
+	log.Debugf("req size sent on the stream, wrote %d bytes on stream", n)
+
+	n, err = s.Write(reqMsg)
+	if err != nil {
+		return err
+	}
+	log.Debugf("req sent, wrote %d bytes on stream", n)
+
+	return nil
+}
+
 // Send an FSRequest for Read action, return a io.Reader to read file from the stream
 func RequestRead(ctx context.Context, s network.Stream, path string, userDID string) (io.Reader, error) {
 
 	fsReq := &FSRequest{DID: userDID, Path: path, Action: ActionType_READ}
 
-	reqMsg, err := proto.Marshal(fsReq)
+	err := sendRequest(fsReq, s)
 	if err != nil {
 		return nil, err
 	}
-
-	n, err := s.Write(reqMsg)
-	if err != nil {
-		return nil, err
-	}
-	log.Debugf("readFile req sent, wrote %d bytes on stream", n)
-
-	err = s.CloseWrite()
-	if err != nil {
-		log.Error("can't close write stream. ", err)
-		s.Reset()
-		return nil, err
-	}
-	log.Debug("stream write closed successfuly")
 
 	pr, pw := io.Pipe()
 
@@ -53,24 +64,10 @@ func RequestRead(ctx context.Context, s network.Stream, path string, userDID str
 func RequestMkDir(ctx context.Context, s network.Stream, path string, userDID string) (string, error) {
 	fsReq := &FSRequest{DID: userDID, Path: path, Action: ActionType_MKDIR}
 
-	reqMsg, err := proto.Marshal(fsReq)
+	err := sendRequest(fsReq, s)
 	if err != nil {
-		return "", err
+		return "", nil
 	}
-
-	n, err := s.Write(reqMsg)
-	if err != nil {
-		return "", err
-	}
-	log.Debugf("readFile req sent, wrote %d bytes on stream", n)
-
-	err = s.CloseWrite()
-	if err != nil {
-		log.Error("can't close write stream. ", err)
-		s.Reset()
-		return "", err
-	}
-	log.Debug("stream write closed successfuly")
 
 	bcid, err := io.ReadAll(s)
 	if err != nil {
@@ -78,6 +75,33 @@ func RequestMkDir(ctx context.Context, s network.Stream, path string, userDID st
 		s.Reset()
 		return "", err
 	}
+
+	return string(bcid), nil
+}
+
+// Send an FSRequest for Write action, wait until receiving a cid for the file
+func RequestWrite(ctx context.Context, s network.Stream, path string, userDID string, f io.Reader) (string, error) {
+	fsReq := &FSRequest{DID: userDID, Path: path, Action: ActionType_WRITE}
+
+	err := sendRequest(fsReq, s)
+	if err != nil {
+		return "", nil
+	}
+
+	if _, err := io.Copy(s, f); err != nil {
+		log.Error("error writing the file on the stream: ", err)
+		return "", err
+	}
+	log.Debug("wrote file on the stream successfuly")
+	s.CloseWrite()
+
+	bcid, err := io.ReadAll(s)
+	if err != nil {
+		log.Error("coudn't receive the cid ", err)
+		s.Reset()
+		return "", err
+	}
+	s.CloseRead()
 
 	return string(bcid), nil
 }
