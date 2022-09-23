e68199e728bcbd13c3192e2941f9b2c37e93b823
Masih H. Derkani <m@derkani.org>
2022-09-23 16:27:36 +0100

    Implement an initial version of pool with pubsub node discovery (#72)
    Implement a basic pool that uses pubsub to discover other nodes. Add
    example to show how the pool is instantiated. Define pubsub announcement
    schema in IPLD.
    
    Tidy up go.mod to follow common format.

---
+++ pool/example_test.go
@@ -0,0 +1,152 @@
+package pool_test
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/functionland/go-fula/pool"
+	logging "github.com/ipfs/go-log/v2"
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/peerstore"
+)
+
+// ExampleNewPool starts a pool named "my-pool" across three nodes, connects two of the nodes to
+// the other one to facilitate a path for pubsub to propagate and shows all three nodes discover
+// each other using pubsub.
+func ExampleNewPool() {
+	const poolName = "my-pool"
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	// Elevate log level to show internal communications.
+	if err := logging.SetLogLevel("*", "info"); err != nil {
+		panic(err)
+	}
+
+	// Use a deterministic random generator to generate deterministic
+	// output for the example.
+	rng := rand.New(rand.NewSource(42))
+
+	// Instantiate the first node in the pool
+	pid1, _, err := crypto.GenerateECDSAKeyPair(rng)
+	if err != nil {
+		panic(err)
+	}
+	h1, err := libp2p.New(libp2p.Identity(pid1))
+	if err != nil {
+		panic(err)
+	}
+	n1, err := pool.New(pool.WithPoolName(poolName), pool.WithHost(h1))
+	if err != nil {
+		panic(err)
+	}
+	if err := n1.Start(ctx); err != nil {
+		panic(err)
+	}
+	defer n1.Shutdown(ctx)
+	fmt.Printf("Instantiated node in pool %s with ID: %s\n", poolName, h1.ID().String())
+
+	// Instantiate the second node in the pool
+	pid2, _, err := crypto.GenerateECDSAKeyPair(rng)
+	if err != nil {
+		panic(err)
+	}
+	h2, err := libp2p.New(libp2p.Identity(pid2))
+	if err != nil {
+		panic(err)
+	}
+	n2, err := pool.New(pool.WithPoolName(poolName), pool.WithHost(h2))
+	if err != nil {
+		panic(err)
+	}
+	if err := n2.Start(ctx); err != nil {
+		panic(err)
+	}
+	defer n2.Shutdown(ctx)
+	fmt.Printf("Instantiated node in pool %s with ID: %s\n", poolName, h2.ID().String())
+
+	// Instantiate the third node in the pool
+	pid3, _, err := crypto.GenerateECDSAKeyPair(rng)
+	if err != nil {
+		panic(err)
+	}
+	h3, err := libp2p.New(libp2p.Identity(pid3))
+	if err != nil {
+		panic(err)
+	}
+	n3, err := pool.New(pool.WithPoolName(poolName), pool.WithHost(h3))
+	if err != nil {
+		panic(err)
+	}
+	if err := n3.Start(ctx); err != nil {
+		panic(err)
+	}
+	defer n3.Shutdown(ctx)
+	fmt.Printf("Instantiated node in pool %s with ID: %s\n", poolName, h3.ID().String())
+
+	// Connect n1 to n2 and n3 so that there is a path for gossip propagation.
+	// Note that we are not connecting n2 to n3 as they should discover
+	// each other via pool's iexist announcements.
+	h1.Peerstore().AddAddrs(h2.ID(), h2.Addrs(), peerstore.PermanentAddrTTL)
+	if err = h1.Connect(ctx, peer.AddrInfo{ID: h2.ID(), Addrs: h2.Addrs()}); err != nil {
+		panic(err)
+	}
+	h1.Peerstore().AddAddrs(h3.ID(), h3.Addrs(), peerstore.PermanentAddrTTL)
+	if err = h1.Connect(ctx, peer.AddrInfo{ID: h3.ID(), Addrs: h3.Addrs()}); err != nil {
+		panic(err)
+	}
+
+	// Wait until the nodes discover each other
+	for {
+		if len(h1.Peerstore().Peers()) == 3 &&
+			len(h2.Peerstore().Peers()) == 3 &&
+			len(h3.Peerstore().Peers()) == 3 {
+			break
+		}
+		select {
+		case <-ctx.Done():
+			break
+		default:
+			time.Sleep(time.Second)
+		}
+	}
+
+	h1Peers := h1.Peerstore().Peers()
+	fmt.Printf("%s peerstore contains %d nodes:\n", h1.ID(), len(h1Peers))
+	for _, id := range h1Peers {
+		fmt.Printf("- %s\n", id)
+	}
+
+	h2Peers := h2.Peerstore().Peers()
+	fmt.Printf("%s peerstore contains %d nodes:\n", h2.ID(), len(h2Peers))
+	for _, id := range h2Peers {
+		fmt.Printf("- %s\n", id)
+	}
+
+	h3Peers := h3.Peerstore().Peers()
+	fmt.Printf("%s peerstore contains %d nodes:\n", h3.ID(), len(h3Peers))
+	for _, id := range h2Peers {
+		fmt.Printf("- %s\n", id)
+	}
+
+	// Unordered output:
+	// Instantiated node in pool my-pool with ID: QmaUMRTBMoANXqpUbfARnXkw9esfz9LP2AjXRRr7YknDAT
+	// Instantiated node in pool my-pool with ID: QmPNZMi2LAhczsN2FoXXQng6YFYbSHApuP6RpKuHbBH9eF
+	// Instantiated node in pool my-pool with ID: QmYMEnv3GUKPNr34gePX2qQmBH4YEQcuGhQHafuKuujvMA
+	// QmaUMRTBMoANXqpUbfARnXkw9esfz9LP2AjXRRr7YknDAT peerstore contains 3 nodes:
+	// - QmaUMRTBMoANXqpUbfARnXkw9esfz9LP2AjXRRr7YknDAT
+	// - QmPNZMi2LAhczsN2FoXXQng6YFYbSHApuP6RpKuHbBH9eF
+	// - QmYMEnv3GUKPNr34gePX2qQmBH4YEQcuGhQHafuKuujvMA
+	// QmPNZMi2LAhczsN2FoXXQng6YFYbSHApuP6RpKuHbBH9eF peerstore contains 3 nodes:
+	// - QmPNZMi2LAhczsN2FoXXQng6YFYbSHApuP6RpKuHbBH9eF
+	// - QmaUMRTBMoANXqpUbfARnXkw9esfz9LP2AjXRRr7YknDAT
+	// - QmYMEnv3GUKPNr34gePX2qQmBH4YEQcuGhQHafuKuujvMA
+	// QmYMEnv3GUKPNr34gePX2qQmBH4YEQcuGhQHafuKuujvMA peerstore contains 3 nodes:
+	// - QmPNZMi2LAhczsN2FoXXQng6YFYbSHApuP6RpKuHbBH9eF
+	// - QmaUMRTBMoANXqpUbfARnXkw9esfz9LP2AjXRRr7YknDAT
+	// - QmYMEnv3GUKPNr34gePX2qQmBH4YEQcuGhQHafuKuujvMA
+}
---
+++ pool/pubsub.go
@@ -0,0 +1,86 @@
+package pool
+
+import (
+	"bytes"
+	_ "embed"
+	"fmt"
+
+	"github.com/ipld/go-ipld-prime"
+	"github.com/ipld/go-ipld-prime/codec/dagcbor"
+	"github.com/ipld/go-ipld-prime/node/bindnode"
+	"github.com/ipld/go-ipld-prime/schema"
+	"github.com/multiformats/go-multiaddr"
+)
+
+const Version0 = "0"
+
+var (
+	PubSubPrototypes struct {
+		Announcement schema.TypedPrototype
+	}
+
+	//go:embed pubsub.ipldsch
+	schemaBytes []byte
+)
+
+type (
+	AnnouncementType int
+	Announcement     struct {
+		Version string
+		Type    AnnouncementType
+		Addrs   []string
+	}
+)
+
+const (
+	UnknownAnnouncementType AnnouncementType = iota
+	IExistAnnouncementType
+)
+
+func init() {
+	typeSystem, err := ipld.LoadSchemaBytes(schemaBytes)
+	if err != nil {
+		panic(fmt.Errorf("cannot load schema: %w", err))
+	}
+	PubSubPrototypes.Announcement = bindnode.Prototype((*Announcement)(nil), typeSystem.TypeByName("Announcement"))
+}
+
+func (a *Announcement) MarshalBinary() ([]byte, error) {
+	n := bindnode.Wrap(a, PubSubPrototypes.Announcement.Type())
+	var buf bytes.Buffer
+	if err := dagcbor.Encode(n, &buf); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func (a *Announcement) SetAddrs(ma ...multiaddr.Multiaddr) {
+	a.Addrs = make([]string, len(ma))
+	for i, m := range ma {
+		a.Addrs[i] = m.String()
+	}
+}
+func (a *Announcement) GetAddrs() ([]multiaddr.Multiaddr, error) {
+	ma := make([]multiaddr.Multiaddr, len(a.Addrs))
+	var err error
+	for i, addr := range a.Addrs {
+		ma[i], err = multiaddr.NewMultiaddr(addr)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return ma, nil
+}
+
+func (a *Announcement) UnmarshalBinary(b []byte) error {
+	buf := bytes.NewBuffer(b)
+	builder := PubSubPrototypes.Announcement.NewBuilder()
+	if err := dagcbor.Decode(builder, buf); err != nil {
+		return err
+	}
+	da := bindnode.Unwrap(builder.Build()).(*Announcement)
+	a.Version = da.Version
+	a.Type = da.Type
+	a.Addrs = da.Addrs
+	return nil
+}
