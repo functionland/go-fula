c48eca659ee5c853a3c75a650fc456c3e0708746
ehsan shariati <ehsan6sha@gmail.com>
2022-12-29 23:33:31 -0500

    removed unused library from example

--- wap/example_test.go
+++ wap/example_test.go
@@ -4,10 +4,7 @@ import (
 	"fmt"
 
 	"github.com/functionland/go-fula/wap"
-	logging "github.com/ipfs/go-log/v2"
 )
-
-var log = logging.Logger("fula/wap")
 
 // ExampleNetworkScan scans the network for available wifis and lists their name with the signal strength
 // forceReload parameter refreshes the list of available wifis on Windows by disabling and enabling the adapter
