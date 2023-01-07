98c2f3468258edf39377447809babfe06e1aa2b6
ehsan shariati <ehsan6sha@gmail.com>
2023-01-07 15:12:02 -0500

    separated callBlockchain from libp2p call
    1- separated callBlockchain from libp2p call
    2- removed callBlockchain from inside go routine

--- blockchain/interface.go
+++ blockchain/interface.go
@@ -12,7 +12,7 @@ type SeededRequest struct {
 
 type Blockchain interface {
 	Start(context.Context) error
-	Seeded(context.Context, interface{}, SeededRequest) ([]byte, error)
+	Seeded(context.Context, peer.ID, SeededRequest) ([]byte, error)
 	SetAuth(context.Context, peer.ID, peer.ID, bool) error
 	Shutdown(context.Context) error
 }
