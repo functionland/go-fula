b4ef42f0b71d8d6355f9691ef8e1e7f5bae9e4b4
ehsan shariati <ehsan6sha@gmail.com>
2023-01-09 19:59:04 -0500

    changed handleSeeded to handleAction and moved to blockchain
    changed handleSeeded to handleAction and moved to blockchain
    
    changed account_sseded to account to contain all account methods
    
    printing type for logging purposes
    
    Update blockchain.go
    
    changed log...w with log.
    
    change switch case to map because the list grows very long
    
    corrected one missing part
    
    switched capitals to small for non public vars
    
    Co-Authored-By: negar-kln <113377124+negar-kln@users.noreply.github.com>
    Co-Authored-By: siroossha <113217130+siroossha@users.noreply.github.com>

--- blockchain/account.go
+++ blockchain/account.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"time"
 
 	"github.com/libp2p/go-libp2p/core/network"
 	"github.com/libp2p/go-libp2p/core/peer"
@@ -42,36 +41,4 @@ func (bl *FxBlockchain) Seeded(ctx context.Context, to peer.ID, r SeededRequest)
 	default:
 		return b, nil
 	}
-}
-
-func (bl *FxBlockchain) handleSeeded(from peer.ID, w http.ResponseWriter, r *http.Request) {
-	log := log.With("action", actionSeeded, "from", from)
-	defer r.Body.Close()
-
-	var p SeededRequest
-	if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
-		log.Debugw("cannot parse request body", "err", err)
-		http.Error(w, "", http.StatusBadRequest)
-		return
-	}
-
-	//go func() {
-	ctx, cancel := context.WithTimeout(context.TODO(), time.Second*30)
-	defer cancel()
-	response, err := bl.callBlockchain(ctx, actionSeeded, p)
-	if err != nil {
-		log.Errorw("failed to process seeded request", "err", err)
-		return
-	}
-	w.WriteHeader(http.StatusAccepted)
-	var res SeededResponse
-	err1 := json.Unmarshal(response, &res)
-	if err1 != nil {
-		log.Errorw("failed to format response", "err", err1)
-	}
-
-	if err := json.NewEncoder(w).Encode(res); err != nil {
-		log.Errorw("failed to write response", "err", err)
-	}
-	//}()
 }
