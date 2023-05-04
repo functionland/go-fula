16f5f87464a1343021d22b915e97ff3331f6f485
ehsan shariati <ehsan6sha@gmail.com>
2023-05-04 19:36:00 -0400

    Added connection creation if not found on start

--- wap/pkg/wifi/wifi.go
+++ wap/pkg/wifi/wifi.go
@@ -166,7 +166,14 @@ func connectToFirstWifi(ctx context.Context, connections []string) error {
 	}
 
 	_, _, err = runCommand(ctx, fmt.Sprintf("nmcli connection up %s passwd-file %s", connection, tempFile.Name()))
-	return err
+
+	if err != nil {
+		DeleteConnection(ctx, connection)
+		createConnection(ctx, connection, connection, passwd)
+		err = connectToNetwork(ctx, connection)
+		return err
+	}
+	return nil
 }
 
 func ConnectToSavedWifi(ctx context.Context) error {
