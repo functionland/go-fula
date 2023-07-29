3ff11ea4374d6491a3a18cc7bf28429056a102fa
Masih H. Derkani <m@derkani.org>
2023-07-30 00:02:54 +0100

    Add initial implementation of local IPFS RPC server
    Add an initial implementation of the IPFS RPC server running locally that serves pin/ls and block/stat.

---
+++ blox/ipfs.go
@@ -0,0 +1,90 @@
+package blox
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-datastore"
+	"github.com/ipfs/go-datastore/query"
+	cidlink "github.com/ipld/go-ipld-prime/linking/cid"
+)
+
+type (
+	pinListResp struct {
+		PinLsList struct {
+			Keys map[string]pinListKeysType `json:"Keys,omitempty"`
+		} `json:"PinLsList"`
+	}
+	pinListKeysType struct {
+		Type string
+	}
+)
+
+func (p *Blox) ServeIpfsRpc() http.Handler {
+	mux := http.NewServeMux()
+	// https://docs.ipfs.tech/reference/kubo/rpc/#api-v0-pin-ls
+	mux.HandleFunc("/api/v0/pin/ls", func(w http.ResponseWriter, r *http.Request) {
+		var resp pinListResp
+		resp.PinLsList.Keys = make(map[string]pinListKeysType)
+		results, err := p.ds.Query(r.Context(), query.Query{
+			KeysOnly: true,
+		})
+		if err != nil {
+			log.Errorw("failed to query datastore", "err", err)
+			http.Error(w, "internal error while querying datastore: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		for result := range results.Next() {
+			if result.Error != nil {
+				log.Errorw("failed to traverse results", "err", err)
+				http.Error(w, "internal error while traversing datastore results: "+err.Error(), http.StatusInternalServerError)
+				return
+			}
+			c, err := cid.Cast([]byte(result.Key))
+			if err != nil {
+				log.Debugw("failed to cast key to cid", "key", result.Key, "err", err)
+				continue
+			}
+			resp.PinLsList.Keys[c.String()] = pinListKeysType{Type: "fx"} //TODO: what should the type be?
+		}
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
+			log.Errorw("failed to encode response to pin ls", "err", err)
+		}
+	})
+	// https://docs.ipfs.tech/reference/kubo/rpc/#api-v0-block-stat
+	mux.HandleFunc("/api/v0/block/stat", func(w http.ResponseWriter, r *http.Request) {
+		c := r.URL.Query().Get("cid")
+		if c == "" {
+			http.Error(w, "no cid specified", http.StatusBadRequest)
+			return
+		}
+		cd, err := cid.Decode(c)
+		if err != nil {
+			http.Error(w, "invalid cid: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+		key := toDatastoreKey(cidlink.Link{Cid: cd})
+		switch value, err := p.ds.Get(r.Context(), key); {
+		case errors.Is(err, datastore.ErrNotFound):
+			http.NotFound(w, r)
+			return
+		case err != nil:
+			http.Error(w, "internal error: "+err.Error(), http.StatusInternalServerError)
+			return
+		default:
+			resp := struct {
+				Key  string `json:"Key"`
+				Size int    `json:"Size"`
+			}{
+				Key:  cd.String(),
+				Size: len(value),
+			}
+			if err := json.NewEncoder(w).Encode(resp); err != nil {
+				log.Errorw("failed to encode response to block stat", "err", err)
+			}
+		}
+	})
+	return mux
+}
