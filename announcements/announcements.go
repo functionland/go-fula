bcdc3e8735424bdb5588f6d04622518904276414
ehsan shariati <ehsan6sha@gmail.com>
2023-11-10 20:24:30 -0500

    corrected announcement tests
    1- We need to stop adding peers to peer store for each received announcements but for now tests fail if we do not
    2- We need to change the blockchain endpoint to 127.0.0.1:4000

--- announcements/announcements.go
+++ announcements/announcements.go
@@ -17,6 +17,7 @@ import (
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/peerstore"
 )
 
 var (
@@ -126,14 +127,15 @@ func (an *FxAnnouncements) HandleAnnouncements(ctx context.Context) {
 			log.Errorw("failed to decode announcement data", "err", err)
 			continue
 		}
-		/*
-			addrs, err := a.GetAddrs()
-			if err != nil {
-				log.Errorw("failed to decode announcement addrs", "err", err)
-				continue
-			}
-			an.h.Peerstore().AddAddrs(from, addrs, peerstore.ConnectedAddrTTL)
-		*/
+
+		//TODO: We do not need to add peers that announce to peerstore
+		addrs, err := a.GetAddrs()
+		if err != nil {
+			log.Errorw("failed to decode announcement addrs", "err", err)
+			continue
+		}
+		an.h.Peerstore().AddAddrs(from, addrs, peerstore.ConnectedAddrTTL)
+
 		log.Infow("received announcement", "from", from, "self", an.h.ID(), "announcement", a)
 		err = an.processAnnouncement(ctx, from, a.Type)
 		if err != nil {
